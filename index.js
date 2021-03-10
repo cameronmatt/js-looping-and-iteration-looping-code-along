7f3fbaae964051bfc67f587db52d7fe6faf0e7b1
Matthew <cameron.matt@outlook.com>
2021-03-10 23:36:24 +1100

    looping and iteration done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+const sinon =  ['Lisa', 'Kaitlin', 'Jan'];
+
+function writeCards(sinon) {
+    const newSinon = [ ];
+    for (let cards = 0; cards < sinon.length; cards++) {
+        newSinon.push(`Thank you, ${sinon[cards]}, for the wonderful surprise gift!`);
+    } 
+    return newSinon;
+}
+
+let callCount = 10;
+function countDown(callCount) {
+    while (callCount >= 0) 
+
+   console.log(callCount--);
+}
+
